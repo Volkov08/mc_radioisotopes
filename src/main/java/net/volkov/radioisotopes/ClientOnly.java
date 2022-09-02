bf0c80e421f6fcff0c377d1d028075be72a85e0a
volkov08 <simonborissevitch@gmail.com>
2022-09-02 21:04:12 +0200

    uranium centrifuge added

--- src/main/java/net/volkov/radioisotopes/ClientOnly.java
+++ src/main/java/net/volkov/radioisotopes/ClientOnly.java
@@ -9,5 +9,6 @@ public class ClientOnly implements ClientModInitializer {
     @Override
     public void onInitializeClient()  {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.URANIUM_GLASS, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.URANIUM_CENTRIFUGE, RenderLayer.getCutout());
     }
 }
