c7eb95fc97f67a2171fe6882efc5aa946105ff9f
volkov08 <simonborissevitch@gmail.com>
2022-09-03 19:22:17 +0200

    made uranium centrifuge double block

--- src/main/java/net/volkov/radioisotopes/block/custom/ModUraniumCentrifugeBlock.java
+++ src/main/java/net/volkov/radioisotopes/block/custom/ModUraniumCentrifugeBlock.java
@@ -2,19 +2,35 @@ package net.volkov.radioisotopes.block.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.enums.DoorHinge;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class ModUraniumCentrifugeBlock extends Block {
-    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
+public class ModUraniumCentrifugeBlock extends ModTallBlock {
 
     public ModUraniumCentrifugeBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(((this.stateManager.getDefaultState()).with(FACING, Direction.NORTH)).with(HALF, DoubleBlockHalf.LOWER));
     }
 
     @Nullable
@@ -36,5 +52,9 @@ public class ModUraniumCentrifugeBlock extends Block {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+        builder.add(HALF);
+    }
+    public PistonBehavior getPistonBehavior(BlockState state) {
+        return PistonBehavior.BLOCK;
     }
 }
