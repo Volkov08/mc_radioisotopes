1c74611d07035b4b20e42aa94e62a3b495872064
volkov08 <simonborissevitch@gmail.com>
2022-09-05 17:31:51 +0200

    added Lead ore and balanced generation

--- src/main/java/net/volkov/radioisotopes/world/feature/ModConfiguredFeatures.java
+++ src/main/java/net/volkov/radioisotopes/world/feature/ModConfiguredFeatures.java
@@ -17,9 +17,19 @@ public class ModConfiguredFeatures {
            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
                    ModBlocks.DEEPSLATE_URANIUM_ORE.getDefaultState()));
 
+    public static final List<OreFeatureConfig.Target> OVERWORLD_LEAD_ORES = List.of(
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
+                    ModBlocks.LEAD_ORE.getDefaultState()),
+            OreFeatureConfig.createTarget(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                    ModBlocks.DEEPSLATE_LEAD_ORE.getDefaultState()));
+
    public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> URANIUM_ORE =
            ConfiguredFeatures.register("uranium_ore", Feature.ORE,
-                   new OreFeatureConfig(OVERWORLD_URANIUM_ORES, 9));
+                   new OreFeatureConfig(OVERWORLD_URANIUM_ORES, 9, 0.6f));
+
+   public static final RegistryEntry<ConfiguredFeature<OreFeatureConfig, ?>> LEAD_ORE =
+           ConfiguredFeatures.register("lead_ore", Feature.ORE,
+                   new OreFeatureConfig(OVERWORLD_LEAD_ORES, 12));
 
    public static void registerConfiguredFeatures() {
        System.out.println("Registriert ModConfiguredFeatures für " + ClientMain.MOD_ID);
--- src/main/java/net/volkov/radioisotopes/world/gen/ModOreGeneration.java
+++ src/main/java/net/volkov/radioisotopes/world/gen/ModOreGeneration.java
@@ -9,5 +9,7 @@ public class ModOreGeneration {
     public static void generateOres() {
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
                 GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.URANIUM_ORE_PLACED.getKey().get());
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(),
+                GenerationStep.Feature.UNDERGROUND_ORES, ModPlacedFeatures.LEAD_ORE_PLACED.getKey().get());
     }
 }
